faf8fc66929a64ac362470aa5e2ab60ac58c5a0b
Malee <N.Khanthamalee@gmail.com>
2024-05-30 14:25:11 +0700

    add switch_case

--- if_else.go
+++ if_else.go
@@ -1,34 +1,34 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	// var score int
-	// fmt.Printf("โปรดกรอบคะแนนของคุณ : ")
-	// fmt.Scanf("%d", &score)
-	// if score >= 50 {
-	// 	fmt.Println("คะแนนคุณคือ : ", score)
-	// 	fmt.Println("ยินดีด้วยคุณสอบผ่าน")
-	// } else {
-	// 	fmt.Println("คะแนนของคุณคือ :", score)
-	// 	fmt.Println("พยายามใหม่นะ คุณสอบไม่ผ่าน")
-	// }
+// func main() {
+// 	// var score int
+// 	// fmt.Printf("โปรดกรอบคะแนนของคุณ : ")
+// 	// fmt.Scanf("%d", &score)
+// 	// if score >= 50 {
+// 	// 	fmt.Println("คะแนนคุณคือ : ", score)
+// 	// 	fmt.Println("ยินดีด้วยคุณสอบผ่าน")
+// 	// } else {
+// 	// 	fmt.Println("คะแนนของคุณคือ :", score)
+// 	// 	fmt.Println("พยายามใหม่นะ คุณสอบไม่ผ่าน")
+// 	// }
 
-	var input int
-	fmt.Println("Let Play a game!")
-	fmt.Printf("ป้อนตัวเลขเข้ามา : ")
-	fmt.Scanf("%d", &input)
-	if input%2 == 0 && input%3 != 0 {
-		fmt.Println("เลข ", input)
-		fmt.Println("ตัวเลขนี้หารด้วยสองลงตัวอย่างเดียวจ้า")
-	} else if input%2 == 0 && input%3 == 0 {
-		fmt.Println("เลข ", input)
-		fmt.Println("ตัวเลขนี้หารด้วยสองและสามลงตัวจ้า")
-	} else if input%2 != 0 && input%3 == 0 {
-		fmt.Println("เลข ", input)
-		fmt.Println("ตัววเลขนี้หารด้วยสามลงตัวจ้า")
-	} else {
-		fmt.Println("เลข ", input)
-		fmt.Println("เลขนี้หารทั้งสองและสามไม่ลงตัว")
-	}
-}
+// 	var input int
+// 	fmt.Println("Let Play a game!")
+// 	fmt.Printf("ป้อนตัวเลขเข้ามา : ")
+// 	fmt.Scanf("%d", &input)
+// 	if input%2 == 0 && input%3 != 0 {
+// 		fmt.Println("เลข ", input)
+// 		fmt.Println("ตัวเลขนี้หารด้วยสองลงตัวอย่างเดียวจ้า")
+// 	} else if input%2 == 0 && input%3 == 0 {
+// 		fmt.Println("เลข ", input)
+// 		fmt.Println("ตัวเลขนี้หารด้วยสองและสามลงตัวจ้า")
+// 	} else if input%2 != 0 && input%3 == 0 {
+// 		fmt.Println("เลข ", input)
+// 		fmt.Println("ตัววเลขนี้หารด้วยสามลงตัวจ้า")
+// 	} else {
+// 		fmt.Println("เลข ", input)
+// 		fmt.Println("เลขนี้หารทั้งสองและสามไม่ลงตัว")
+// 	}
+// }
---
+++ switch_case.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Println("ระบุตัวเลขเพื่อดำเนินการ โดย")
+	fmt.Println("พิมพ์ 1 เพื่อเปืดบัญชีธนาคาร")
+	fmt.Println("พิมพ์ 2 เพื่อฝาก-ถอนเงิน")
+	fmt.Println("พิมพ์เลขอื่นๆ เพื่อติดต่อพนักงาน")
+	fmt.Printf("ระบุตัวเลข : ")
+	fmt.Scanf("%d", &number)
+
+	switch number {
+	case 1:
+		fmt.Println("เลข", number, "เปิดบัญชีธนาคาร กรุณารอสักครู่...")
+	case 2:
+		fmt.Println("เลข", number, "ฝาก-ถอนเงิน กรุณารอสักครู่...")
+	default:
+		fmt.Println("เลข", number, "ติดต่อพนักงาน กรุณารอสักครู่...")
+	}
+
+}
