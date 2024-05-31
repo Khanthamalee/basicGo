7351449ee88b0157797f8338d896179703cd423c
Malee <N.Khanthamalee@gmail.com>
2024-05-31 10:47:58 +0700

    add for range

--- for_loop.go
+++ for_loop.go
@@ -1,18 +1,19 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	fmt.Println("For_loop!!!!!")
-	fmt.Println("Let 's fizzbuzz!!!!!")
+// func main() {
+// 	fmt.Println("For_loop!!!!!")
+// 	fmt.Println("Let 's fizzbuzz!!!!!")
 
-	for i := 1; i <= 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("Fizzbuzz!", i)
-		} else if i%3 == 0 && i%5 != 0 {
-			fmt.Println("Fizz!", i)
-		} else if i%3 != 0 && i%5 == 0 {
-			fmt.Println("Buzz!", i)
-		}
-	}
-}
+// 	for i := 1; i <= 100; i++ {
+// 		if i%3 == 0 && i%5 == 0 {
+// 			fmt.Println("Fizzbuzz!", i)
+// 		} else if i%3 == 0 && i%5 != 0 {
+// 			fmt.Println("Fizz!", i)
+// 		} else if i%3 != 0 && i%5 == 0 {
+// 			fmt.Println("Buzz!", i)
+// 		}
+// 	}
+
+// }
